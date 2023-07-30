c9683436d0880d2642b54c1ac1668c1c9a2a2fe2
Abhishek Roy <139121367+Abhishek-coder2001@users.noreply.github.com>
2023-07-30 11:18:53 +0530

    Add files via upload
    This is the Java Script File

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById('inputNumber');
+let buttons = document.querySelectorAll('button');
+let maths=document.querySelectorAll('math')
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+
+        }
+
+        else if (e.target.innerHTML == 'C') {
+            string = "";
+            input.value = string;
+        }
+
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+
+
+    })
+})
